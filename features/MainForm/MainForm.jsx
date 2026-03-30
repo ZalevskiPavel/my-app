2df393b071a913ae3a96e1b812769d4dfb9b83b9
Zalevski Pavel <zalevskiipavel112@gmail.com>
2026-03-30 09:22:15 +0300

    added function(form can now get user data)

--- features/MainForm/MainForm.jsx
+++ features/MainForm/MainForm.jsx
@@ -1,3 +1,5 @@
+"use client"
+import { useState } from "react";
 import styles from "./main-form-styles.module.css";
 import { Bellefair, Montserrat } from "next/font/google";
 
@@ -13,45 +15,87 @@ const montserrat = Montserrat({
 
 
 export default function MainForm() {
-  return (
-    <div className={styles.formContainer}>
-      <h2 className={bellefair.className}>ADD PROJECT</h2>
+    const [formData, setFormData] = useState({
+        id : "", 
+        nume : "",
+        descriere: "",
+        status: "nou",
+        deadline: ""
+    });
 
-      <form className={bellefair.className} >
-        <div className={styles.formGroup}>
-          <label>ID</label>
-          <input type="text" name="id" required className={montserrat.className}/>
+    function somethingChanged(e){
+        const {name, value} = e.target;
+
+        setFormData(prev => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    function AddNewNote(e){
+        e.preventDefault();
+        console.log(formData);
+    }
+    return (
+        <div className={styles.formContainer}>
+            <h2 className={bellefair.className}>ADD PROJECT</h2>
+
+            <form className={bellefair.className} onSubmit={AddNewNote}>
+                <div className={styles.formGroup}>
+                    <label>ID</label>
+                    <input 
+                    type="text" 
+                    name="id" 
+                    required 
+                    className={montserrat.className} 
+                    onChange={somethingChanged}/>
+                </div>
+
+                <div className={styles.formGroup}>
+                    <label>Nume</label>
+                    <input 
+                    type="text" 
+                    name="nume" 
+                    required 
+                    className={montserrat.className} 
+                    onChange={somethingChanged}/>
+                </div>
+
+                <div className={styles.formGroup}>
+                    <label>Descriere</label>
+                    <textarea 
+                    name="descriere" 
+                    className={montserrat.className}
+                    onChange={somethingChanged}></textarea>
+                </div>
+
+                <div className={styles.formGroup}>
+                    <label>Status</label>
+                    <select 
+                    name="status" 
+                    className={montserrat.className}
+                    onChange={somethingChanged}>
+                        <option value="nou">Nou</option>
+                        <option value="in_progres">In Progress</option>
+                        <option value="finalizat">Finalizat</option>
+                    </select>
+                </div>
+
+                <div className={styles.formGroup}>
+                    <label>Deadline</label>
+                    <input 
+                    type="date" 
+                    name="deadline" 
+                    required 
+                    className={montserrat.className} 
+                    onChange={somethingChanged}/>
+                </div>
+
+                <button type="submit" className={styles.btn}>
+                    Salvează
+                </button>
+            </form>
         </div>
-
-        <div className={styles.formGroup}>
-          <label>Nume</label>
-          <input type="text" name="nume" required className={montserrat.className}/>
-        </div>
-
-        <div className={styles.formGroup}>
-          <label>Descriere</label>
-          <textarea name="descriere" className={montserrat.className}></textarea>
-        </div>
-
-        <div className={styles.formGroup}>
-          <label>Status</label>
-          <select name="status" className={montserrat.className}>
-            <option value="nou">Nou</option>
-            <option value="in_progres">In Progress</option>
-            <option value="finalizat">Finalizat</option>
-          </select>
-        </div>
-
-        <div className={styles.formGroup}>
-          <label>Deadline</label>
-          <input type="date" name="deadline" required className={montserrat.className}/>
-        </div>
-
-        <button type="submit" className={styles.btn}>
-          Salvează
-        </button>
-      </form>
-    </div>
-  );
+    );
 }
 
